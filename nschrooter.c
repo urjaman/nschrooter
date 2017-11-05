37db0cf4fdb1905c4ce9dff9d4b59fad495e16e9
Urja Rannikko <urjaman@gmail.com>
2017-11-05 21:26:57 +0200

    cleanup, make the user-only case work a little better
    Apparently you cant mess much with devnodes or such anyways
    as a not-actually-root user, so the best i can do
    for you is symlink dev (and sys, cant mount that either) from
    the host.

--- nschrooter.c
+++ nschrooter.c
@@ -11,23 +11,34 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <sys/sysmacros.h>
 
-void perror_msg_and_die2(const char* msg, const char *extra) {
+static void perror_msg_and_die2(const char* msg, const char *extra) {
 	if (extra) fprintf(stderr,"%s: ", extra);
 	perror(msg);
 	exit(1);
 }
 
-void perror_msg_and_die(const char* msg) {
+static void perror_msg_and_die(const char* msg) {
 	perror_msg_and_die2(msg, NULL);
 }
 
-void error_msg_and_die(const char* msg) {
+static void error_msg_and_die(const char* msg) {
 	fprintf(stderr,"%s\n", msg);
 	exit(1);
 }
 
-void procwritef(const char * fn, const char *msg, ...) {
+
+static int pwritef(const char * fn, const char *buf) {
+        int fd = open(fn, O_WRONLY);
+        if (fd < 0) return -1;
+        if (write(fd, buf, strlen(buf)) != strlen(buf))
+        	return -1;
+        close(fd);
+        return 0;
+}
+
+static void procwritef(const char * fn, const char *msg, ...) {
 	char buf[80];
         va_list ap;
         va_start(ap, msg);
@@ -35,12 +46,9 @@ void procwritef(const char * fn, const char *msg, ...) {
 		error_msg_and_die("procwritef buf overflow");
 	}
         va_end(ap);
-        int fd = open(fn, O_WRONLY);
-        if (fd < 0) perror_msg_and_die2("procwritef open", fn);
-        if (write(fd, buf, strlen(buf)) != strlen(buf))
-        	perror_msg_and_die2("procwritef write", fn);
-        close(fd);
+        if (pwritef(fn, buf) != 0) perror_msg_and_die2("procwritef", fn);
 }
+
 
 int main(int argc, char **argv) {
 	if (argc < 3) {
@@ -70,8 +78,6 @@ int main(int argc, char **argv) {
 	procwritef("/proc/self/uid_map", "0 %d 1", muid);
 	procwritef("/proc/self/gid_map", "0 %d 1", mgid);
 
-	//procwritef("/proc/sys/kernel/hostname", "%s", hn);
-
 	/* slave mount */
 	if (mount(NULL, "/", NULL, MS_REC|MS_PRIVATE, NULL) != 0)
 		perror_msg_and_die("slave mount");
@@ -80,13 +86,11 @@ int main(int argc, char **argv) {
 	if (mount(path, path, NULL, MS_BIND|MS_REC, NULL) != 0)
 		perror_msg_and_die("bind mount");
 
-	//if (mount(NULL, path, NULL, MS_REC|MS_SHARED, NULL) != 0)
-	//	perror_msg_and_die("shared mount");
-
 	/* This chdir is necessary to change the current directory to the bind-mounted fs. */
 	if (chdir(path) != 0)
 		perror_msg_and_die("chdir(path)");
 
+	/* Make the old rootfs visible. We need them later, and as an user we cant unmount them either.  */
 	(void) mkdir("oldroot", 0777);
 
 	if (mount("/", "./oldroot", NULL, MS_BIND|MS_REC, NULL) != 0)
@@ -103,17 +107,37 @@ int main(int argc, char **argv) {
 
 	/* We need to f**k it to be in the new pid namespace. */
 	pid_t chld = fork();
-	if (chld == -1)
-		perror_msg_and_die("fork");
+	if (chld == -1) perror_msg_and_die("fork");
+
 	if (chld) {
+		/* I suppose we need to wait for the child. */
 		wait(NULL);
 		exit(0);
 	}
 
-	/* One more mount namespace please. */
-//	if (unshare(CLONE_NEWNS) != 0)
-//		perror_msg_and_die("unshare2");
+	/* We are basically in the environment we need, on the rest
+	 * of things just report errors instead of aborting on error */
 
-	execvp(argv[2], argv+2);
+	if (mount("proc", "/proc", "proc", 0, NULL) != 0)
+		perror("mount /proc");
+
+
+	unlink("dev"); rmdir("dev");
+	unlink("sys"); rmdir("sys");
+
+	if (symlink("/oldroot/dev", "dev") != 0)
+		perror("dev symlink");
+
+	if (symlink("/oldroot/sys", "sys") != 0)
+		perror("sys symlink");
+
+	if (sethostname(hn, strlen(hn)) != 0)
+		perror("sethostname");
+
+	char *termp = getenv("TERM");
+	if (termp) termp -= strlen("TERM=");
+	char * const env[] = { "PATH=/bin:/sbin:/usr/bin:/usr/sbin", termp, NULL };
+
+	execvpe(argv[2], argv+2, env);
 	perror_msg_and_die("execvp");
 }
